73e45ccfdb4f412acf4c6bc83b7074c532e39181
Alejandro Ruiz Diaz <alee_ruizdiaz@hotmail.com>
2026-05-16 15:05:51 -0300

    estructura inicial del proyecto

---
+++ src/main/java/integrador/prog2/Main.java
@@ -0,0 +1,12 @@
+package integrador.prog2;
+
+/**
+ *
+ * @author alee_
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
